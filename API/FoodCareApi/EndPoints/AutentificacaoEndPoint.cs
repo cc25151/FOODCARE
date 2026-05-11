2e970e865f4e98e8a05fdeefa5e10da22385456a
JoaoMarcos00 <cc25144@g.unicamp.br>
2026-05-11 11:29:21 -0300

    .

---
+++ API/FoodCareApi/EndPoints/AutentificacaoEndPoint.cs
@@ -0,0 +1,61 @@
+//Para fins de organização, foi criado um EndPoint específio para a autentificação de um login
+
+using Microsoft.EntityFrameworkCore;
+using FoodCareApi.Data;
+using FoodCareApi.Models;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace FoodCareApi.Endpoints;
+
+public static class AutenticacaoEndpoints
+{
+    public static void MapAutenticacaoEndpoints(this WebApplication app)
+    {
+        app.MapPost("/login", async (Usuario usuarioLogin, AppDbContext db, IConfiguration config) =>
+        {
+            //Proucura um usuário por email e senha
+            var usuario = await db.Usuarios
+                .FirstOrDefaultAsync(u => u.Email == usuarioLogin.Email && u.Senha == usuarioLogin.Senha);
+
+            //Se não encontrar, retorna Não Autorizado (código http 401)
+            if (usuario is null) 
+                return Results.Unauthorized();
+
+            //Se encontrar, prepara a geração do Token JWT
+            var tokenHandler = new JwtSecurityTokenHandler();
+            
+            // Pega a chave secreta 
+            var chaveSecreta = "chaveSecreta";
+            var chaveBytes = Encoding.ASCII.GetBytes(chaveSecreta); // Pega o número de bytes da chave secreta
+
+            // 4. Define as informações que vão dentro do Token Reinvidicações
+            var DescricaoToken = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(new[]
+                {
+                    new Claim(ClaimTypes.Name, usuario.Nome),
+                    new Claim(ClaimTypes.Email, usuario.Email),
+                    new Claim("id", usuario.IdUsuario.ToString()),
+                    new Claim("tipo", usuario.TipoPessoa) // Útil para saber se é PF ou PJ no Front-end
+                }),
+                Expires = DateTime.UtcNow.AddHours(3), // Token vale por 3 horas
+                SigningCredentials = new SigningCredentials(
+                    new SymmetricSecurityKey(chaveBytes), 
+                    SecurityAlgorithms.HmacSha256Signature)
+            };
+
+            // 5. Cria o Token e envia para o usuário
+            var token = tokenHandler.CreateToken(DescricaoToken);
+            var tokenString = tokenHandler.WriteToken(token);
+
+            return Results.Ok(new 
+            { 
+                usuario = usuario.Nome, 
+                token = tokenString 
+            });
+        });
+    }
+}
