38e72764d5686e25767c9e32e33d10ccf267c007
JoaoMarcos00 <cc25144@g.unicamp.br>
2026-05-15 12:18:07 -0300

    .

--- API/FoodCareApi/obj/Debug/net10.0/FoodCareApi.AssemblyInfo.cs
+++ API/FoodCareApi/obj/Debug/net10.0/FoodCareApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FoodCareApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6a7f892d844c159324f29f7a9e63c61690298afc")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c7acbc20f505fc8d947a7bd6d13579fbb6c8c5b3")]
 [assembly: System.Reflection.AssemblyProductAttribute("FoodCareApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FoodCareApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
