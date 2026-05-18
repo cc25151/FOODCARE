8eb71194ee63794be0f919918336a67fb99e0996
JoaoMarcos00 <cc25144@g.unicamp.br>
2026-05-18 11:33:24 -0300

    Hash da senha, criação do programs

--- API/FoodCareApi/Program.cs
+++ API/FoodCareApi/Program.cs
@@ -1,6 +1,38 @@
+using Microsoft.EntityFrameworkCore;
+using FoodCareApi.Data;
+using FoodCareApi.Endpoints;
+
 var builder = WebApplication.CreateBuilder(args);
+
+// 1. CONFIGURAÇÃO DE SERVIÇOS
+
+// Configura o Swagger (Tela de testes das rotas)
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+// Configura a conexão com o seu Banco de Dados (SQL Server)
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+// Ativa o Swagger 
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
 
+// Redireciona requisições HTTP para HTTPS automaticamente
+app.UseHttpsRedirection();
+
+
+
+app.MapUsuarioEndpoints(); // Registra as rotas de Usuários 
+app.MapAutenticacaoEndpoints(); // Registra as rotas de Login/Autenticação
+app.MapDoadorEndpoints(); // Registra as rotas doador
+app.
+
+// Deixa a API ativa e escutando o aplicativo Android
 app.Run();
