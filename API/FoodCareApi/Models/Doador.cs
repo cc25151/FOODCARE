0bbae0e25c2a182c2c3c24ecb1fa22624a410081
JoaoMarcos00 <cc25144@g.unicamp.br>
2026-05-22 17:32:47 -0300

    .

--- API/FoodCareApi/Models/Doador.cs
+++ API/FoodCareApi/Models/Doador.cs
@@ -3,6 +3,6 @@ namespace FoodCareApi.Models;
 public class Doador{
         public int idDoador      { get; set; }
         public int idUsuario     { get; set; }
-        public Usuario usuarioDoador  { get; set; } = null!; //Usado para acessar os atributos do usuário
+        public Usuario usuarioDoador  { get; set; } = null!; //Remover
         public decimal pontuacao { get; set; }
     }
