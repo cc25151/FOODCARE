e8b30dfbd21a8faa70c5bd3980be59f6b026db13
Sakamoto69 <cc25151@g.unicamp.br>
2026-05-27 14:18:43 -0300

    ;

--- FoodCare/app/src/main/java/com/example/foodcare/data/repository/GeocodingRepository.kt
+++ FoodCare/app/src/main/java/com/example/foodcare/data/repository/GeocodingRepository.kt
@@ -6,17 +6,15 @@ class GeocodingRepository(
     private val api: GeocodingApi
 ) {
 
-    suspend fun getCoordinates(address: String): Pair<Double, Double>? {
+    suspend fun coordenadas(address: String): Pair<Double, Double>? {
+
         val result = api.searchAddress(address)
 
-        if (result.isNotEmpty()) {
-            val first = result[0]
-            return Pair(
-                first.lat.toDouble(),
-                first.lon.toDouble()
+        return result.firstOrNull()?.let {
+            Pair(
+                it.lat.toDouble(),
+                it.lon.toDouble()
             )
         }
-
-        return null
     }
 }
